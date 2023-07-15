5c2400ef706d6a8af55e4368902ed46ecb131494
Miguel-D3vgit config --global user.name Miguel-D3vgit config --global user.email xmiguelxpeixotox@gmail.com <xmiguelxpeixotox@gmail.com>
2023-07-15 11:13:12 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import routes from "./routes/route.js";
 import cors from "cors";
 import helmet from "helmet";
 
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes)
+app.use((error,req,res,next)=>{
+    res.status(error.httpStatusCode).json("Bad Request");
+ });
+
 
 export default app
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,83 @@
 import express from "express";
 import modelHero from "../models/modelHero.js";
 
+
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-   res.json({ message: "Testando" });
+// Listar todos os herois
+
+routes.get("/heroes",async(req,res,next) => {
+   try{
+   const result = await modelHero.findAll();
+   res.json({
+       "message":"Sucessfuly",
+       "heroes": result
+   })
+  }catch(err){
+   const error = new Error(err);
+   error.httpStatusCode=400;
+   return next(error);
+  }
 });
 
-routes.get("/heroes",async(req,res) => {
-   const result = await modelHero.findAll();
-   res.json(result)
-})
+
+// Buscar heroi cujo nome igual a endpoint
+
+routes.get("/heroes/:name",async(req,res,next) => {
+     try{
+    const heroName = await modelHero.findOne({ where:{ name:req.params.name }});
+
+     if(heroName === null){
+      res.json("Heroi nao encontrado");
+     } else{
+    res.json({
+      "message":"Sucessfuly",
+      "hero":heroName
+   })}
+   }catch(err){
+     const error = new Error(err);
+     error.httpStatusCode=400;
+     return next(error);
+  }
+});
+
+// Buscar heroi pelo id
+
+routes.get("/:id",async(req,res,next) => {
+    try{
+    const hero = await modelHero.findByPk(req.params.id);
+
+     if(hero === null){
+      res.json("Heroi nao encontrado tente outro id")
+    }else{
+    res.json({
+     "message":"Sucessfuly",
+     "hero": hero
+    })}
+  } catch(err){
+    const error = new Error(err);
+    error.httpStatusCode=400;
+    return next(error);
+   }
+});
+
+// Buscar heroi por funcao(role)
+
+routes.get("/roles/:role",async(req,res,next) => {
+   try{
+   const heroRole = await modelHero.findAll({ where:{ roles:req.params.role }});
+    if(heroRole.length === 0){
+      res.json("Nenhum heroi encontrado para essa funcao")
+    }else{
+   res.json({
+     "message":"Sucessfuly",
+     "heroes": heroRole
+   })}
+   } catch(err){
+    const error = new Error(err);
+    error.httpStatusCode=400;
+    return next(error);
+   }
+});
+
 export default routes
