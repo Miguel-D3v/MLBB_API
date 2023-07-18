6065b5d105c4036f0bd8a7dabd7b2a6b8751d3da
Miguel-D3vgit config --global user.name Miguel-D3vgit config --global user.email xmiguelxpeixotox@gmail.com <xmiguelxpeixotox@gmail.com>
2023-07-18 11:32:48 -0300

    set db

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv/config.js";
 
  const dbURL= process.env.DATABASE_URL;
 
- const sequelize = new Sequelize(dbURL);
+ const sequelize = new Sequelize(dbURL,{
+ dialectModule: require('pg')
+ });
 
 export default sequelize ;
